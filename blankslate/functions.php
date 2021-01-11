7e7e2c61dfda0d35e026af6b96d5764a8a9e4d03
NicolineRusbak <1081543@ucn.dk>
2021-01-11 11:23:16 +0100

    Revert "Update"
    This reverts commit 863236f43ef18252aa6efebbdb214f8d89c475fa.

--- blankslate/functions.php
+++ blankslate/functions.php
@@ -1,44 +1,20 @@
 <?php
-// add_action( 'after_setup_theme', 'blankslate_setup' );
-// function blankslate_setup() {
-// load_theme_textdomain( 'blankslate', get_template_directory() . '/languages' );
-
-// Add theme support
+add_action( 'after_setup_theme', 'blankslate_setup' );
+function blankslate_setup() {
+load_theme_textdomain( 'blankslate', get_template_directory() . '/languages' );
 add_theme_support( 'title-tag' );
 add_theme_support( 'automatic-feed-links' );
 add_theme_support( 'post-thumbnails' );
-add_theme_support( 'post_format', ['aside', 'gallery', 'link', 'image', 'quote', 'status', 'video', 'audio', 'chat'] );
-add_theme_support( 'html5' );
-// add_theme_support( 'html5', array( 'search-form' ) );
-add_theme_support( 'custom-background' );
-add_theme_support( 'custom-header' );
-add_theme_support( 'custom-logo' );
-add_theme_support( 'customize-selecctive-refresh-widgets' );
-
-// global $content_width;
-// if ( ! isset( $content_width ) ) { $content_width = 1920; }
-// register_nav_menus( array( 'main-menu' => esc_html__( 'Main Menu', 'blankslate' ) ) );
-// }
-
-// Load in our CSS
-function antiquistema_enqueue_styles() {
-    wp_enqueue_style( 'main-css', get_stylesheet_directory_uri() . '/style.css', [], time(), 'all' );
-    wp_enqueue_script( 'jquery' );
+add_theme_support( 'html5', array( 'search-form' ) );
+global $content_width;
+if ( ! isset( $content_width ) ) { $content_width = 1920; }
+register_nav_menus( array( 'main-menu' => esc_html__( 'Main Menu', 'blankslate' ) ) );
 }
-add_action( 'wp_enqueue_scripts', 'antiquistema_enqueue_styles' );
-
-
-
-
-
-// add_action( 'wp_enqueue_scripts', 'blankslate_load_scripts' ); er skrevet om ovenover
-// function blankslate_load_scripts() {
-// wp_enqueue_style( 'blankslate-style', get_stylesheet_uri() );
-// wp_enqueue_script( 'jquery' );
-// }
-
-// ----- ALT HERUNDER ER IKKE ÆNDRET I ------ FRA BLANKSLATE  --------
-
+add_action( 'wp_enqueue_scripts', 'blankslate_load_scripts' );
+function blankslate_load_scripts() {
+wp_enqueue_style( 'blankslate-style', get_stylesheet_uri() );
+wp_enqueue_script( 'jquery' );
+}
 add_action( 'wp_footer', 'blankslate_footer_scripts' );
 function blankslate_footer_scripts() {
 ?>
