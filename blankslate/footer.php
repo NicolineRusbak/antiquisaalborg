5b26170b11f6bd398b7f50a7a52a26f16f8377c0
1082204 <57616789+1082204@users.noreply.github.com>
2021-01-12 11:30:21 +0100

    Footer test

--- blankslate/footer.php
+++ blankslate/footer.php
@@ -1,7 +1,24 @@
 </div>
 <footer id="footer">
+<hr>
 <div id="copyright">
 &copy; <?php echo esc_html( date_i18n( __( 'Y', 'blankslate' ) ) ); ?> <?php echo esc_html( get_bloginfo( 'name' ) ); ?>
+</div>
+<div>
+    <h3>Sociale medier</h3>
+    Facebook logo her
+    <p>@antiquisaalborg</p>
+    Instagram logo her
+    <p>@antiquis_aalborg</p>
+</div>
+<div class="column">
+    <h3>OM</h3>
+    <a href="#">KONTAKT</a>
+    <a href="#">OM ANTIQUIS</a>
+    <a href="#">COOKIES</a>
+</div>
+<div>
+    <p>CVR: 123456789</p>
 </div>
 </footer>
 </div>
