113b9fda9f621f3efdfbf55871cb493305f4b5a7
NicolineRusbak <1081543@ucn.dk>
2021-01-13 13:59:02 +0100

    Alt i forhold til Blog

--- blankslate/archive.php
+++ blankslate/archive.php
@@ -1,13 +1,23 @@
 <?php get_header(); ?>
+
 <main id="content">
-<header class="header">
-<h1 class="entry-title"><?php single_term_title(); ?></h1>
-<div class="archive-meta"><?php if ( '' != the_archive_description() ) { echo esc_html( the_archive_description() ); } ?></div>
-</header>
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<?php get_template_part( 'entry' ); ?>
-<?php endwhile; endif; ?>
-<?php get_template_part( 'nav', 'below' ); ?>
+
+    <header class="header">
+        <h1><?php single_cat_title(); ?></h1>
+        <div class="archive-meta"><?php if ( '' != the_archive_description() ) { echo esc_html( the_archive_description() ); } ?></div>
+    </header>
+
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+        <h3><?php the_title(); ?></h3>
+        <?php the_excerpt(); ?>
+        <a href="<?php the_permalink(); ?>">Læs mere... </a>
+
+    <?php endwhile; endif; ?>
+
+    <p> Template: archieve.php </p>
+
 </main>
+
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
--- blankslate/category.php
+++ blankslate/category.php
@@ -1,13 +1,17 @@
 <?php get_header(); ?>
+
 <main id="content">
-<header class="header">
-<h1 class="entry-title"><?php single_term_title(); ?></h1>
-<div class="archive-meta"><?php if ( '' != the_archive_description() ) { echo esc_html( the_archive_description() ); } ?></div>
-</header>
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<?php get_template_part( 'entry' ); ?>
-<?php endwhile; endif; ?>
-<?php get_template_part( 'nav', 'below' ); ?>
+
+    <header class="header">
+        <h1 class="entry-title"><?php single_cat_title(); ?></h1>
+        <div class="archive-meta"><?php if ( '' != the_archive_description() ) { echo esc_html( the_archive_description() ); } ?></div>
+    </header>
+    
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+        <?php get_template_part( 'entry' ); ?>
+    <?php endwhile; endif; ?>
+
 </main>
+<p> Template: categories </p>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
