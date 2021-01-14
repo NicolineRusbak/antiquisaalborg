a6b498622a8328c1a6903a2d9d1957148e12ad2e
NicolineRusbak <1081543@ucn.dk>
2021-01-14 11:51:48 +0100

    Page.php herobilleder

--- blankslate/page.php
+++ blankslate/page.php
@@ -4,21 +4,27 @@
 
     <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
         <header class="header">
+            <div class="hero-image">
+                <?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>
+                
+                <h1 class="entry-title"><?php the_title(); ?></h1> 
 
-            <h1 class="entry-title"><?php the_title(); ?></h1> <?php edit_post_link(); ?>
+            </div>
+
+            <?php edit_post_link(); ?>
 
         </header>
 
         <div class="entry-content">
 
-            <?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>
-                <?php the_content(); ?>
+            <?php the_content(); ?>
 
-            <div class="entry-links"><?php wp_link_pages(); ?></div>
-            
         </div>
     </article>
     <?php endwhile; endif; ?>
+
+    <p> Template: page.php </p>
+    
 </main>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
