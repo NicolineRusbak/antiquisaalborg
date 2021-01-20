b9d5c79203ecbde8c3f4f3bb0c72f60de49fb5c1
NicolineRusbak <1081543@ucn.dk>
2021-01-20 10:11:22 +0100

    footer

--- blankslate/footer.php
+++ blankslate/footer.php
@@ -1,23 +1,35 @@
-</div>
-<footer id="footer">
-<div>
-    <h3>Sociale medier</h3>
-    <i class="fab fa-facebook-square"></i>
-    <p>@antiquisaalborg</p>
-    <i class="fab fa-instagram"></i>
-    <p>@antiquis_aalborg</p>
-</div>
-<div class="footer-column">
-    <h3>OM</h3>
-    <a href="#">KONTAKT</a>
-    <a href="#">OM ANTIQUIS</a>
-    <a href="#">COOKIES</a>
-</div>
-<div>
-    <p>CVR: 123456789</p>
-</div>
-</footer>
-</div>
-<?php wp_footer(); ?>
-</body>
+            </div>
+            <footer id="footer">
+                <div>
+                    <h2>Sociale medier</h2>
+
+                    <a href="https://www.facebook.com/antiquisaalborg">
+                        <i class="fab fa-facebook-square"></i>
+                        <p>@antiquisaalborg</p>
+                    </a>
+                    
+                    <a href="https://www.instagram.com/antiquis_aalborg/">
+                        <i class="fab fa-instagram"></i>
+                        <p>@antiquis_aalborg</p>
+                    </a>
+
+                </div>
+
+                <div class="footer-column">
+                    <h2>Om</h2>
+                    <a href="<?php echo site_url('/kontakt'); ?>">KONTAKT</a>
+                    <a href="<?php echo site_url('/om-antiquis'); ?>">OM ANTIQUIS</a>
+                    <a href="#">COOKIES</a>
+                    <p>CVR: </p>
+                </div>
+
+            </footer>
+        </div>
+        <?php wp_footer(); ?>
+        
+        <div id="copyright">
+            &copy; <?php echo esc_html( date_i18n( __( 'Y', 'antiquistema' ) ) ); ?> <?php echo esc_html( get_bloginfo( 'name' ) ); ?>
+        </div>
+
+    </body>
 </html>
